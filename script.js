d15eda54d9f5e493d7e43207375f42e6009db894
bilipeaches <taoxichen2008@gmail.com>
2021-05-22 15:08:43 +0800

    add-code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $(".lt").html("<img src='Loading.svg' alt='Loading'>📋正在加载...");
     $.get("body.html", function(date, status){
+        alert(status)
         if(status == "sucess"){
-            alert(data)
             $(".html").html(date);
             $(".html").ready(function(){
                 $(".loading").hide();

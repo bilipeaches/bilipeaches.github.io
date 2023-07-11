406a8f2bd043baf07c163448b7788c0f5e39f47e
bilipeaches <bilipeaches@126.com>
2023-07-11 17:05:47 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bilipeaches.github.io
+
+为自己的项目做一个索引
--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".lt").html("<img src='Loading.svg' alt='Loading'>📋正在加载...");
-    $(".html").load("body.html");
-    $(".html").ready(function(){
-        $(".loading").hide();
-    });
-});
